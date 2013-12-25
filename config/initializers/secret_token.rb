9fafefbec699e1c87374732f49b3412003a97baa
Riveascore <agaxx010@morris.umn.edu>
2013-12-25 15:36:21 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThrowBreakTrainer::Application.config.secret_key_base = 'd26cf8b99eb7bfe31378dc7b8f7771e3dda408dd17e9826c675109359c8355fb392d7d0ea31844d4180e8061e9847b715c50fffc4e898ff95420fa4cf8119fe6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThrowBreakTrainer::Application.config.session_store :cookie_store, key: '_ThrowBreakTrainer_session'
